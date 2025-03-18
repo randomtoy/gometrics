61981d2d4fb160fc813a00419a523635cab16d5f
Yuri Sergeev <randomtoy@gmail.com>
2025-03-18 22:46:23 +0300

    fix ctx and db queries

--- internal/db/helpers.go
+++ internal/db/helpers.go
@@ -2,7 +2,6 @@ package db
 
 import "database/sql"
 
-// Конвертирует sql.NullFloat64 → *float64
 func toFloat64Ptr(n sql.NullFloat64) *float64 {
 	if n.Valid {
 		return &n.Float64
@@ -10,7 +9,6 @@ func toFloat64Ptr(n sql.NullFloat64) *float64 {
 	return nil
 }
 
-// Конвертирует sql.NullInt64 → *int64
 func toInt64Ptr(n sql.NullInt64) *int64 {
 	if n.Valid {
 		return &n.Int64
