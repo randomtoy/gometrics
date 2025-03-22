3bf4b650dca39622c2909395628b4c60ea4698da
Yuri Sergeev <randomtoy@gmail.com>
2025-03-23 02:39:47 +0300

    iter14

--- internal/crypto/middleware.go
+++ internal/crypto/middleware.go
@@ -19,15 +19,16 @@ func HMACSHA256Middleware(secretKey string) echo.MiddlewareFunc {
 			defer c.Request().Body.Close()
 
 			// agentKey := ""
-			receivedHash := c.Request().Header.Get("HashSHA256")
-			expectedHash := ComputeHMACSHA256(string(body), secretKey)
-			fmt.Printf("Server received: '%s'\nExpected hash: %s\nGot hash: %s\n",
-				c.Request().Body, expectedHash, receivedHash)
+			if c.Request().Header.Get("HashSHA256") != "" {
+				expectedHash := ComputeHMACSHA256(string(body), secretKey)
+				receivedHash := ComputeHMACSHA256(string(body), c.Request().Header.Get("HashSHA256"))
+				fmt.Printf("Server received: '%v'\nExpected hash: %s\nGot hash: %s\n",
+					c.Request().Header, expectedHash, receivedHash)
 
-			if receivedHash != expectedHash {
-				return c.JSON(http.StatusBadRequest, echo.Map{"error": "Хеш подписи не совпадает"})
+				if receivedHash != expectedHash {
+					return c.JSON(http.StatusBadRequest, echo.Map{"error": "Хеш подписи не совпадает"})
+				}
 			}
-
 			c.Request().Body = io.NopCloser(bytes.NewReader(body))
 
 			return next(c)
