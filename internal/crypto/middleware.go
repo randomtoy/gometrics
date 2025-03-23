b2108d4893c5dcb49cdc54e53df688fd397012fa
Yuri Sergeev <randomtoy@gmail.com>
2025-03-23 10:07:00 +0300

    iter14

--- internal/crypto/middleware.go
+++ internal/crypto/middleware.go
@@ -2,7 +2,6 @@ package crypto
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"net/http"
 
@@ -12,6 +11,10 @@ import (
 func HMACSHA256Middleware(secretKey string) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
+			if c.Request().Method != http.MethodPost {
+				return next(c)
+			}
+
 			body, err := io.ReadAll(c.Request().Body)
 			if err != nil {
 				return c.JSON(http.StatusInternalServerError, echo.Map{"error": "Ошибка чтения тела запроса"})
@@ -20,8 +23,6 @@ func HMACSHA256Middleware(secretKey string) echo.MiddlewareFunc {
 
 			receivedHash := c.Request().Header.Get("HashSHA256")
 			expectedHash := ComputeHMACSHA256(string(body), secretKey)
-			fmt.Printf("Server received: '%s'\nExpected hash: %s\nGot hash: %s\n",
-				c.Request().Header.Get("HashSHA256"), expectedHash, receivedHash)
 
 			if receivedHash != expectedHash {
 				return c.JSON(http.StatusBadRequest, echo.Map{"error": "Хеш подписи не совпадает"})
